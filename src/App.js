66cebe23bd541e8f4e2888a7c9f7afa81b3a41a3
Md Salman <md.salman@northsouth.edu>
2020-11-09 22:57:19 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import './App.css'
+import "./App.css";
 
 const App = () => {
   let [file, setFile] = useState([]);
@@ -7,32 +7,44 @@ const App = () => {
 
   const uploadMultipleFiles = (e) => {
     file = [];
-    file = [...file,e.target.files];
-    setFile (file);
+    file = [...file, e.target.files];
+    setFile(file);
     for (let i = 0; i < file[0].length; i++) {
-      imgUrl = [...imgUrl,(URL.createObjectURL(file[0][i]))];
+      imgUrl = [...imgUrl, URL.createObjectURL(file[0][i])];
     }
     setImgUrl(imgUrl);
-
   };
 
   const deleteFiles = (e) => {
     e.preventDefault();
-    imgUrl =[];
-    setImgUrl(imgUrl)
+    imgUrl = [];
+    setImgUrl(imgUrl);
   };
 
+  const deleteSingleImg = (e) => {
+    e.preventDefault();
+    imgUrl = imgUrl.filter((item) => item !== e.target.id);
+    setImgUrl(imgUrl);
+  };
+
+  let length = imgUrl.length;
 
   return (
     <form>
-      <div>
-        <input type="file" onChange={uploadMultipleFiles} multiple />
+
+      <div >
+        <h1>Upload Some Images From Your Local Drive</h1>
+        <input style={{display:"block", margin:"auto",textAlign:"center"}} type="file" onChange={uploadMultipleFiles} multiple /> <br/>
+        {length > 0 && <button style={{ position: "fixed"}} type="button" onClick={deleteFiles}>Remove All Image </button> } <br/> <br/> 
       </div>
-      <button type="button" onClick={deleteFiles}>Remove Image Preview</button>
+
       <div className="img-container">
         {(imgUrl || []).map((url) => (
-          <img src={url} alt="..." />
-        ))}  
+          <div className="single-img">
+            <img src={url} alt="..." /> <br />
+            <button id={url} onClick={deleteSingleImg}>Delete</button>
+          </div>
+        ))}
       </div>
     </form>
   );
